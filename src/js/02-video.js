523b8e58fa5d8a8ac00c1c8534dc968a81bff6a2
Pavlo Yakubovskyi <pavloyakubovskyi@gmail.com>
2023-05-06 20:52:40 +0200

    add task02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'videoplayer-current-time';
+
+const iframeEL = document.querySelector('iframe');
+const player = new Vimeo.Player(iframeEL);
+
+const savedTime = JSON.parse(localStorage.getItem(STORAGE_KEY));
+
+if (savedTime) {
+  player.setCurrentTime(savedTime.seconds);
+}
+
+player.on('timeupdate', throttle(onPlayerStart, 1000));
+
+function onPlayerStart(e) {
+  localStorage.setItem(STORAGE_KEY, JSON.stringify({ seconds: e.seconds }));
+}
